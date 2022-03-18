7c44d6bf9223953357c5abc7c1542a5ed086d96b
rniochy <lniochy@gmail.com>
2022-03-18 12:15:02 +0100

    adding  the frontend in this projet

--- database/bdConection.js
+++ database/bdConection.js
@@ -1,13 +1,16 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config();
 
+const URI = process.env.URI
 const uri = 'mongodb://localhost:27017/user'
 mongoose.connect(uri,{
       useNewUrlParser: true,
       useUnifiedTopology: true,
-})
+});
 
 const connection = mongoose.connection
 connection.once('open',()=>{
      console.log("Conectado com a bd ")
-})
+});
--- server.js
+++ server.js
@@ -2,22 +2,21 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 
-import './database/bdConection.js'
 import routerUser  from './controller/usuario.js'
 import routerProject from './controller/projecto.js'
 import routerTask from './controller/tarefa.js'
+import './database/bdConection.js'
 
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 const port = process.env.PORT || 3001
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use('/user', routerUser)
-
-app.use('/project', routerProject)
-app.use('/task', routerTask)
+app.use('/user', routerUser);
+app.use('/project', routerProject);
+app.use('/task', routerTask);
 
 app.listen(port, ()=>{
        console.log("SERVER RUNNING ...")
